c1d6f557c1b6cabed9f525ac291697f18d57064c
Gavin Garza <gaving@live.com>
2020-01-23 00:05:16 +0900

    Updating for Heroku deployment

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,7 +6,27 @@ class DosesController < ApplicationController
   end
 
   def create
-
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    if @dose.destroy
+      redirect_to cocktail_path(:cocktail_id)
+    else
+      render :show
+    end
+  end
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
